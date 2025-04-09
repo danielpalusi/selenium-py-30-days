35e2b04dbf5523ea9296bf34f1cd08bb143b8ffe
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-09 09:51:43 +0700

    [Day 03] ✅ Learn PyTest structure and create first test

--- tests/test_open_browser.py
+++ tests/test_open_browser.py
@@ -1,14 +1,12 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-service_object = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service_object)
-
-driver.get("https://www.saucedemo.com/")
-print(driver.title)
-
-time.sleep(3)
-
-driver.quit()
+def test_open_web():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.saucedemo.com")
+    assert "sauce" in driver.title
+    time.sleep(3)
+    driver.quit()
