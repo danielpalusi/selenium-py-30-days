092ca20602f701837b53f87acfbe36aec12bc962
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-09 11:36:31 +0700

    [Day 03] ✅ Use assert and PyTest options (markers, -v, -s, -m)

--- tests/test_open_browser.py
+++ tests/test_open_browser.py
@@ -4,9 +4,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+def supposed():
+    return False
+
+@pytest.mark.smokez
 def test_open_web():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.saucedemo.com")
-    assert "sauce" in driver.title
+    assert "Swag" in driver.title
+    assert supposed() == True
+
     time.sleep(3)
     driver.quit()
