430bf547e8ec2164006b0ad5f3b74ecc5aac77fa
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-10 13:55:43 +0700

    [Day 04] ✅ Add a config file for URLs, browser, credentials (yaml)

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,4 +5,6 @@ def test_login(driver):
     login_page = LoginPage(driver)
     login_page.go_to("https://www.saucedemo.com")
     login_page.login("standard_user", "secret_sauce")
+
+    assert "inventory" in driver.current_url
     time.sleep(3)
