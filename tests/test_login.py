e11881b6333ce429af5cdcae6ea1373923b7a333
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-11 10:08:38 +0700

    [Day 5] ✅ Implement config reader from yaml

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,14 +4,16 @@ from utils.config_parser import config
 
 def test_login(driver):
     login_page = LoginPage(driver)
-    login_page.go_to("https://www.saucedemo.com")
-    login_page.login("standard_user", "secret_sauce")
-
-    assert "inventory" in driver.current_url
 
     # return is dict, so we can easily access it
     # dict like object in js
-    print(config['base_url'])
-    print(config['password'])
+    url = config['base_url']
+    username = config['username']
+    password = config['password']
+
+    login_page.go_to(url)
+    login_page.login(username, password)
+
+    assert "inventory" in driver.current_url
 
     time.sleep(3)
