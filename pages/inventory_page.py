c071df30221febe38717a22930f1197a6a1619d5
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-12 22:46:28 +0700

    [Day 6] ✅ Add more test for Inventory page

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,8 +6,9 @@ class InventoryPage(BasePage):
         super().__init__(driver)
 
     #Locators
-    add_to_chart_button = (By.ID, "add-to-cart-sauce-labs-backpack")
+    backpack_add_to_cart_button = (By.ID, "add-to-cart-sauce-labs-backpack")
     product_name = (By.CLASS_NAME, "inventory_item_name")
+    cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
 
     def get_item_title(self):
         """
@@ -20,8 +21,15 @@ class InventoryPage(BasePage):
         """
         return [item.text for item in self.driver.find_elements(*self.product_name)]
 
-    # def add_to_chart(self):
-    #     """
-    #     Perform add product to cart by click the "Add to chart" button
-    #
-    #     """
+    def add_to_chart(self):
+        """
+        Perform add product to cart by click the "Add to chart" button
+
+        No parameter
+
+        Return:
+            Cart badge amount
+        """
+        self.click(self.backpack_add_to_cart_button)
+        chart_badge = self.driver.find_element(*self.cart_badge)
+        return chart_badge.text
--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -20,7 +20,26 @@ def test_retrieve_inventory_product_name(driver):
 
     result = inventory_page.get_item_title()
 
+    print(result)
+
     assert len(result) > 0
 
     logger.info("Inventory test - Retrieve product name finished ⛳⛳")
+
+def test_add_product_to_chart(driver):
+    # this code DRY
+    logger.info("Starting inventory test - Retrieve product name 🚀🚀")
+
+    login_page = LoginPage(driver)
+    inventory_page = InventoryPage(driver)
+
+    url = config['base_url']
+    username = config['username']
+    password = config['password']
+
+    login_page.go_to(url)
+    login_page.login(username, password)
+
+    cart_badge_amount = inventory_page.add_to_chart()
+    assert int(cart_badge_amount) > 0
 
