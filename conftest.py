c14d4ce755bcda5066298b748fa02e83cb1d0909
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-17 10:28:07 +0700

    [Day 10] ✅ Add command-line browser switch support

--- conftest.py
+++ conftest.py
@@ -2,15 +2,44 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from utils.config_parser import config
 from utils.logger import create_logger
 from utils.screenshot import take_screenshot
 
+def create_driver(browser_name):
+    """
+    Map the browser based on passed browser name
+
+    Parameter:
+    browser_name (string)
+
+    Return value:
+    webdriver
+    """
+
+    if browser_name == "edge":
+        return webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    elif browser_name == "chrome":
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    else:
+        raise ValueError(f"Browser {browser_name} is not supported yet")
+
+# no need to put @pytest.hookimpl() if there's no parameter added 💡💡
+@pytest.hookimpl()
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser to run with"
+    )
 
 @pytest.fixture
-def driver():
+def browser_name(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture
+def driver(browser_name):
     # Setup
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = create_driver(browser_name)
 
     # Provide the driver to the test
     yield driver
@@ -39,6 +68,7 @@ def retrieve_config_data():
 
 logger = create_logger(__name__)
 
+# built-in hook, this will be triggered before and after test
 @pytest.hookimpl()
 def pytest_runtest_protocol(item, nextitem):
     logger.info(f"Starting test: {item.nodeid} 🚀🚀")
@@ -49,6 +79,7 @@ def pytest_runtest_protocol(item, nextitem):
 
     return result
 
+# built-in hook, this will be triggered after test
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     # Before test execution
