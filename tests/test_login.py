2da8b9f1c44eb193dd151046d9c990287c9c36a3
daniel-panggabean <daniel.panggabean@yourpay.co.id>
2025-04-17 09:21:39 +0700

    [Day 10] ✅ Add negative tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,20 @@
+import pytest
+
 from pages.login_page import LoginPage
-import time
 from utils.logger import create_logger
 
 logger = create_logger(__name__)
 
 def test_login(driver, retrieve_config_data):
+    """
+    Perform login process, go to url then insert credentials
+
+    Parameter:
+    driver
+    retrieve_config_data (text)
+
+    No return
+    """
     login_page = LoginPage(driver)
 
     url, username, password = retrieve_config_data
@@ -14,9 +24,23 @@ def test_login(driver, retrieve_config_data):
 
     assert "inventory" in driver.current_url
 
-    # Simulate: Check for success, ONLY mock test
-    success = False
-    if success:
-        logger.info("Login successful ✅")
-    else:
-        logger.warning("Login may have failed. UI did not respond as expected ⚠️")
+@pytest.mark.negative
+def test_invalid_login(driver,retrieve_config_data):
+    """
+    Perform login process, go to url then insert invalid credentials
+
+    Parameter:
+    driver
+    retrieve_config_data (text)
+
+    No return
+    """
+
+    login_page = LoginPage(driver)
+
+    # need to use the parameterize method
+    url, _, _ = retrieve_config_data
+    login_page.go_to(url)
+    login_page.login("username", "password")
+
+    assert "inventory" not in driver.current_url
