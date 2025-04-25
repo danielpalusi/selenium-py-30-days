1753f1bd9f1678da80c9cc8109338f408ee45792
daniel-panggabean <danielbosputera@gmail.com>
2025-04-25 16:25:01 +0700

    [Day 11] ✅ Add headless browser support

--- conftest.py
+++ conftest.py
@@ -7,21 +7,45 @@ from utils.config_parser import config
 from utils.logger import create_logger
 from utils.screenshot import take_screenshot
 
-def create_driver(browser_name):
+def add_headless_option(options, browser_name):
+    """
+        Add function to add headless option, will be called by create driver function
+
+        Parameter:
+        options
+        browser_name (string)
+
+        Return value:
+        options
+        """
+
+    if browser_name == "chrome" or browser_name == "edge":
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+    return options
+
+def create_driver(browser_name, headless=False):
     """
     Map the browser based on passed browser name
 
     Parameter:
     browser_name (string)
+    headless (boolean)
 
     Return value:
     webdriver
     """
 
     if browser_name == "edge":
-        return webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+        options = webdriver.EdgeOptions()
+        if headless:
+            options = add_headless_option(options, browser_name)
+        return webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
     elif browser_name == "chrome":
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        options = webdriver.ChromeOptions()
+        if headless:
+            options = add_headless_option(options, browser_name)
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     else:
         raise ValueError(f"Browser {browser_name} is not supported yet")
 
@@ -31,6 +55,8 @@ def pytest_addoption(parser):
     parser.addoption(
         "--browser", action="store", default="chrome", help="Browser to run with"
     )
+    parser.addoption("--headless", action="store_true", help="Run browser in headless mode")
+
 
 @pytest.fixture
 def browser_name(request):
